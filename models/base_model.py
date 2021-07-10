3989372a1cdfb9fa15fe10fdf472f870e649b3c6
MariumAZ <mariem.azouz@dauphine.tn>
2021-07-10 22:14:04 +0100

    create basemodel class

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!usr/bin/env python3
+
+import datetime
+import uuid
+
+
+class BaseModel():
+    def __init__(self) -> None:
+        self.id =str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now() 
+    def __str__(self):
+        """
+        custom str function
+        should print: 
+        [<class name>] (<self.id>) <self.__dict__>
+        """    
+        return "[{}], ({}), {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def save(self):
+        """
+        updates the public instance attribute :
+        updated_at with the current date time
+        """    
+        self.updated_at = datetime.datetime.now()
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values
+        Goal : serialization/desuerialization
+        """
+        dict = self.__dict__
+        dict['__class__'] = self.__class__.__name__
+        for k, v in dict.items():
+            if k == 'created_at' or k == 'updated_at':
+                dict[k] = datetime.datetime.isoformat(v)
+        return dict
+                
+
