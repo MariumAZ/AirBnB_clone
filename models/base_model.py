d4da3483b7169ea7046323fbcaf8739f74cd8743
MariumAZ <mariem.azouz@dauphine.tn>
2021-09-16 13:43:27 +0100

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,11 @@
 #usr/bin/env python3
-
+"""
+contains class BaseModel
+"""
 import uuid
 from datetime import datetime
 import time
+
 
 class BaseModel():
     """"
@@ -12,21 +15,18 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self) -> str:
         info = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__) 
         return info
-    def save(self): 
+
+    def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
-    def to_dict(self):  
+
+    def to_dict(self): 
         dic = self.__dict__
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
         dic['updated_at'] = self.updated_at.isoformat()
         return dic
-        
-
-
-
-
-
