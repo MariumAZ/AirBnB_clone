a75d8e906fc920d2767e6b1589923b393a4b510c
MariumAZ <mariem.azouz@dauphine.tn>
2021-09-17 10:55:50 +0100

    fixing model from, dict

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,12 @@ class BaseModel():
             for k, v in kwargs.items():
                 if k in ['created_at', 'updated_at']:
                     v = datetime.strptime(v, time_format)
-                if k!="__class__":
-                    setattr(self,k, v)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                if k!= "__class__":
+                    setattr(self, k, v)
+        else:            
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         info = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__) 
@@ -30,11 +31,10 @@ class BaseModel():
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
-        pass
 
     def to_dict(self): 
         """ returns dictionary """
-        dic = self.__dict__.copy()
+        dic = self.__dict__
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
         dic['updated_at'] = self.updated_at.isoformat()
