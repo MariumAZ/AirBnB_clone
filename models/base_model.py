a2262f27015844b7cd47f83c6f91e5dfb65bf112
MariumAZ <mariem.azouz@dauphine.tn>
2021-09-17 10:46:14 +0100

    create basemodel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel():
                 if k in ['created_at', 'updated_at']:
                     v = datetime.strptime(v, time_format)
                 if k!="__class__":
-                    setattr(k, v)
+                    setattr(self,k, v)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
