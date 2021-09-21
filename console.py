882f78051845c09a22000ddd3f1c02f05d70dd9b
MariumAZ <mariem.azouz@dauphine.tn>
2021-09-21 09:11:19 +0100

    add console

---
+++ console.py
@@ -0,0 +1,27 @@
+#! usr/bin/env python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    intro = "-------------- Welcome to AirBnB console --------------  "
+    prompt = '(hbnb) '
+   
+    def do_quit(self, arg):
+        """ Quit command to exit the program """
+        return True
+    def do_EOF(self, arg):
+        """ Exits console """
+        return True
+    def emptyline(self) -> bool:
+        return False    
+    def do_help(self, arg: str) :
+        """ help methods"""
+        return super().do_help(arg)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
+
+
+
+
